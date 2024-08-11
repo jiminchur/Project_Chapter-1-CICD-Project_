a93bd77d3dbb2eb27d68b42b63ba81d1135a0a69
jiminchur <ttlcc13@naver.com>
2024-08-12 02:47:01 +0900

    Initial commit

---
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -0,0 +1,79 @@
+package com.sparta.myselectshop.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.security.UserDetailsImpl;
+import com.sparta.myselectshop.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ProductController {
+
+    private final ProductService productService;
+    
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return productService.createProduct(requestDto, userDetails.getUser());
+    }
+
+    // 관심 상품 희망 최저가 등록하기
+    @PutMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+        return productService.updateProduct(id, requestDto);
+    }
+
+    @GetMapping("/products")
+    public Page<ProductResponseDto> getProducts(
+        @RequestParam("page") int page,
+        @RequestParam("size") int size,
+        @RequestParam("sortBy") String sortBy,
+        @RequestParam("isAsc") boolean isAsc,
+        @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return productService.getProducts(userDetails.getUser(),  page-1, size, sortBy, isAsc);
+    }
+
+    @PostMapping("/products/{productId}/folder")
+    public void addFolder(
+        @PathVariable Long productId, 
+        @RequestParam Long folderId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+        ){
+            productService.addFolder(productId,folderId,userDetails.getUser());
+    }
+
+    @GetMapping("/folders/{folderId}/products")
+    public Page<ProductResponseDto> getProductsInFolder(
+        @PathVariable Long folderId,
+        @RequestParam("page") int page,
+        @RequestParam("size") int size,
+        @RequestParam("sortBy") String sortBy,
+        @RequestParam("isAsc") boolean isAsc,
+        @AuthenticationPrincipal UserDetailsImpl userDetails){
+            return productService.getPorductsInFolder(
+                folderId,
+                page-1,
+                size,
+                sortBy,
+                isAsc,
+                userDetails.getUser()
+            );
+        }
+}
---
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -0,0 +1,139 @@
+package com.sparta.myselectshop.service;
+
+import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.entity.ProductFolder;
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.entity.UserRoleEnum;
+import com.sparta.myselectshop.exception.ProductNotFoundException;
+import com.sparta.myselectshop.naver.dto.ItemDto;
+import com.sparta.myselectshop.repository.FolderRepository;
+import com.sparta.myselectshop.repository.ProductFolderRepository;
+import com.sparta.myselectshop.repository.ProductRepository;
+
+import java.util.Locale;
+import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    
+
+    private final ProductRepository productRepository;
+    private final FolderRepository folderRepository;
+    private final ProductFolderRepository productFolderRepository;
+    private final MessageSource messageSource;
+
+    public static final int MIN_MY_PRICE = 100;
+
+    public ProductResponseDto createProduct(ProductRequestDto requestDto, User user){
+        Product product = productRepository.save(new Product(requestDto, user));
+        return new ProductResponseDto(product);
+    }
+    @Transactional
+    public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto){
+        int myprice = requestDto.getMyprice();
+        if( myprice < MIN_MY_PRICE ){
+            throw new IllegalArgumentException(
+                messageSource.getMessage(
+                    "below.min.my.price",
+                    new Integer[]{MIN_MY_PRICE},
+                    "Worng Price",
+                    Locale.getDefault()
+                )
+            );
+        }
+
+        Product product = productRepository.findById(id).orElseThrow(() ->
+        new ProductNotFoundException(messageSource.getMessage(
+            "not.found.product",
+            null,
+            "Not Found Product",
+            Locale.getDefault()
+        ))
+        );
+        
+        product.update(requestDto);
+
+        return new ProductResponseDto(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductResponseDto> getProducts(User user,int page, int size, String sortBy, boolean isAsc) {
+        // 페이징 처리
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        UserRoleEnum userRoleEnum = user.getRole();
+
+        Page<Product> productList;
+
+        if (userRoleEnum == UserRoleEnum.USER) {
+            // 사용자 권한이 USER 일 경우
+            productList = productRepository.findAllByUser(user, pageable);
+        } else {
+            productList = productRepository.findAll(pageable);
+        }
+
+        return productList.map(ProductResponseDto::new);
+    }
+
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto){
+        Product product = productRepository.findById(id).orElseThrow(() ->
+            new NullPointerException("해당 상품은 존재하지 않습니다.")
+        );
+        product.updateByItemDto(itemDto);
+    }
+    public void addFolder(Long productId, Long folderId, User user) {
+        Product product = productRepository.findById(productId).orElseThrow(
+            ()-> new NullPointerException("해당 상품이 존재하지 않습니다.")
+        );
+        
+        Folder folder = folderRepository.findById(folderId).orElseThrow(
+            () -> new NullPointerException("해당 폴더가 존재하지 않습니다.")
+        );
+
+        if (!product.getUser().getId().equals(user.getId()) || !folder.getUser().getId().equals(user.getId())){
+            throw new IllegalArgumentException("회원님의 관시상품이 아니거나, 회원님의 폴더가 아닙니다.");
+        }
+
+
+        Optional<ProductFolder> overlabFolder =  productFolderRepository.findByProductAndFolder(product, folder);
+    
+        if (overlabFolder.isPresent()){
+            throw new IllegalArgumentException("중복된 폴더 입니다.");
+        }
+    
+        productFolderRepository.save(new ProductFolder(product, folder));
+    }
+    public Page<ProductResponseDto> getPorductsInFolder(Long folderId, int page, int size, String sortBy, boolean isAsc,
+            User user) {
+        // 페이징 처리
+            Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+            Sort sort = Sort.by(direction, sortBy);
+            Pageable pageable = PageRequest.of(page, size, sort);
+
+            // 해당 폴더에 등록된 상품을 가져옵니다.
+            Page<Product> products = productRepository.findAllByUserAndProductFolderList_FolderId(user, folderId, pageable);
+
+            Page<ProductResponseDto> responseDtoList = products.map(ProductResponseDto::new);
+
+            return responseDtoList;
+        }
+
+
+}
